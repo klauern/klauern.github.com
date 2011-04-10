95d0428037229da7d11045a05105c94e25a6321d
Klauer - Nick Klauer <nicholasklauer@alliantenergy.com>
2011-04-10 15:32:35 -0500

    Using HAML since I like it better than mucking with HTML

--- config.rb
+++ config.rb
@@ -1,12 +1,13 @@
 # Require any additional compass plugins here.
 # Set this to the root of your project when deployed:
+project_type = :stand_alone
 http_path = "/"
 css_dir = "css"
 sass_dir = "src"
-images_dir = "img"
+images_dir = "images"
 javascripts_dir = "js"
 # To enable relative paths to assets via compass helper functions. Uncomment:
-relative_assets = true
+#relative_assets = true
 
 # Uncomment the following line to get a compacted css file 
-# output_style = :compressed
+output_style = :compressed
